e66bc617d7a57053c503cf8f9f82be7d1c63d785
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-06-07 10:21:15 -0300

    jogo1

--- src/pctjogo/javasenhajogo.java
+++ src/pctjogo/javasenhajogo.java
@@ -1,8 +1,44 @@
 package pctjogo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class javasenhajogo {
     public static void main(String[] args) {
-        System.out.println("que");
+        int senha[] = new int[6];
+        int tentativa[] = new int[6];
+        System.out.println("   ");
+        System.out.println("BEM VINDO AO JOGO MASTER MIND!");
+        System.out.println("insira, em ordem, os números da sua tentativa abaixo.");
+        System.out.println(" ");
+        gennum(senha);
+        scantentativa(tentativa);
+        verify(senha, tentativa);
     }
 
+    public static void gennum(int v[]) {
+        Random rd = new Random();
+        for (int i = 0; i < v.length; i++) {
+            v[i] = rd.nextInt(0, 9);
+        }
+    }
+
+ public static void scantentativa (int v[]){
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < v.length; i++) {
+v[i]=sc.nextInt();
+ }
 }
+
+public static boolean verify (int v[], int c[]){
+    for (int i = 0; i < v.length; i++) {
+    if ( v[i] == c[i]) {
+        return true;
+    }else{
+        System.out.println("errou a senha ne seu broxa");
+        return false;
+    }
+}
+}
+}
+
